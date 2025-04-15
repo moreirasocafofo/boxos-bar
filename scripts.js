7cdccf0db4964253c7973780272f8d248774f003
moreirasocafofo <alexandre.moreira.lima@escola.pr.gov.br>
2025-04-15 15:07:18 +0000

    contraste

--- scripts.js
+++ scripts.js
@@ -1,20 +1,36 @@
 document.addEventListener('DOMContentLoaded', function(){
-    const botaoAumentarFonte = document.getElementById('aumentar-fonte')
+    const botaoAumentar = document.getElementById("aumentar-fonte")
     //Pega o botão de aumentar fonte e coloca na constante
 
     let tamanhoDaFonte = 1
     // define o tamanho da fonte na variável
 
-    botaoAumentarFonte.addEventListener('click', function(){
+    botaoAumentar.addEventListener('click', function(){
         tamanhoDaFonte = tamanhoDaFonte + 0.1
         document.body.style.fontSize = `${tamanhoDaFonte}rem`
+
+    })
+  
+    const botaoDiminuir = document.getElementById("diminuir-fonte")
+    
+        botaoDiminuir.addEventListener('click', function(){
+            tamanhoDaFonte = tamanhoDaFonte - 0.1
+            document.body.style.fontSize = `${tamanhoDaFonte}rem`
     })
 
-    const botaoDiminuirFonte = document.getElementById('diminuir-fonte')
-    //Pega o botão de diminuir fonte e coloca na constante
+    const botaoAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesAcessibilidade = document.getElementById('opcoes-acessibilidade')
+    
+    botaoAcessibilidade.addEventListener('click', function (){
+        botaoAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesAcessibilidade.classList.toggle('apresenta-lista')
+       })
+       
 
-    botaoDiminuirFonte.addEventListener('click', function(){
-        tamanhoDaFonte = tamanhoDaFonte - 0.1
-        document.body.style.fontSize = `${tamanhoDaFonte}rem`
-    })
+       const alternaContraste = document.getElementById('alterna-contraste')
+       alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste')
+
+        
+})
 })
